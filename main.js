4689fce5fbc498f6ad3faa460de55c641bda65b5
Efendi <tanefendi.97@gmail.com>
2023-01-28 23:35:47 +0700

    update functionality

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gameBoard=(()=>{
-  const gameBoardArray=[];
+  let gameBoardArray=[];
   gameBoardArray.length=9;
   let turn='o';
   for(let i=0;i<gameBoardArray.length;i++){
@@ -26,7 +26,7 @@ const gameBoard=(()=>{
           gameBoardArray[2]==='x'&&gameBoardArray[4]==='x'&&gameBoardArray[6]==='x'
         ){
           console.log('x win');
-          board.style.display='none';
+          congratulate('x');
         }else if(gameBoardArray[0]==='o'&&gameBoardArray[1]==='o'&&gameBoardArray[2]==='o'||
           gameBoardArray[3]==='o'&&gameBoardArray[4]==='o'&&gameBoardArray[5]==='o'||
           gameBoardArray[6]==='o'&&gameBoardArray[7]==='o'&&gameBoardArray[8]==='o'||
@@ -37,11 +37,32 @@ const gameBoard=(()=>{
           gameBoardArray[2]==='o'&&gameBoardArray[4]==='o'&&gameBoardArray[6]==='o'
         ){
           console.log('o win');
-          board.style.display='none';
+          congratulate('o');
         }
       }
     })
   }
+  const info= document.querySelector('.info');
+  const playAgainButton = document.querySelector('.playAgain');
+  congratulate = (turn)=>{
+      info.textContent=`Congrats! ${turn} win!`;
+      playAgainButton.style.display='block';
+    }
+  reset = ()=>{
+    gameBoardArray=[];
+    turn='o';
+    const gameBoardCell = document.querySelectorAll('.board div');
+    for(let i=0;i<gameBoardCell.length;i++){
+      gameBoardCell[i].textContent='';
+    }
+    info.textContent='';
+  }
+  return {reset}
 })();
 
-
+const playAgainButton = document.querySelector('.playAgain');
+playAgainButton.addEventListener('click',playAgain)
+function playAgain(){
+  gameBoard.reset()
+  playAgainButton.style.display='none';
+}
